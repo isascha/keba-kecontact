10eac29de68379279f45832d13fd1d2729bf5f89
isascha <134978599+isascha@users.noreply.github.com>
2024-05-31 14:31:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="keba_kecontact",
-    version="3.0.3",
+    version="3.0.3i",
     author="Philipp Danner",
     author_email="philipp@danner-web.de",
     description="A python library to communicate with the KEBA charging stations via udp",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dannerph/keba-kecontact",
+    url="https://github.com/isascha/keba-kecontact",
     packages=setuptools.find_packages(),
     install_requires=["asyncio_dgram>=2.1.1", "netifaces>=0.11.0"],
     classifiers=[
